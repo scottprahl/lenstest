5c59680cf4b7daad5c0743b08718d03470dd3700
Scott Prahl <scott@prahls.com>
2022-12-11 13:23:23 -0800

    add saggita tests for perfect conics

--- tests/test_lenstest.py
+++ tests/test_lenstest.py
@@ -70,6 +70,51 @@ class TestLensTest(unittest.TestCase):
       result = lenstest.lenstest.circle_polygon(*x)
 #      self.assertEqual(result, expected_output)
 
+  def test_sagitta_values(self):
+    """
+    Test calculations against Table 1 in Benjamin & Rosenblum.
+    
+    --- "Radii of Curvature and Sagittal Depths of Conic Sections,"
+    ICLC, Vol. 19, pp. 76-83, March/April 1992
+    
+    The eccentricity for the first column was changed from -0.45 to 0.504j
+    this gives the values of sagitta at all heights.
+    
+    I believe that there was a typo for results[8,3].  This is fixed.
+    """
+    e = np.array([0.504j,0,0.45,1,2])
+    conic_constant = (-e*e).real
+
+    # table from the paper
+    results = np.array([
+    [1.0,0.016,0.016,0.016,0.016,0.016],
+    [2.0,0.064,0.064,0.064,0.064,0.063],
+    [3.0,0.146,0.146,0.145,0.144,0.140],
+    [4.0,0.262,0.261,0.260,0.256,0.245],
+    [5.0,0.414,0.412,0.409,0.401,0.374],
+    [6.0,0.606,0.600,0.595,0.577,0.524],
+    [7.0,0.842,0.829,0.820,0.785,0.693],
+    [8.0,1.128,1.104,1.086,1.026,0.878],
+    [9.0,1.472,1.429,1.400,1.298,1.076],
+    [10.0,1.890,1.813,1.761,1.603,1.285],
+    [11.0,2.403,2.269,2.183,1.939,1.504],
+    [12.0,3.061,2.816,2.673,2.308,1.731]])
+    
+    #Sagittal Depths Given for a Spherical Surface Having a Radius of Curvature of 7.80 mm, 
+
+    RoC = 7.8
+    y = (results[:,0]/2).flatten()
+    x = np.zeros_like(y)
+    
+    for i, K in enumerate(conic_constant):
+      sag = lenstest.lenstest.sagitta(RoC, K, x, y)
+      print(sag)
+      table_sag = (results[:,i+1]).flatten()
+      print(table_sag)
+      np.testing.assert_allclose(sag, table_sag, atol=3e-3, rtol=3e-3)
+
+
+
 # Run the test case
 if __name__ == '__main__':
   unittest.main()
