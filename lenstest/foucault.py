9b570e305303bc68fa570fc2ca0f83b7001a81dc
Scott Prahl <scott@prahls.com>
2023-01-02 12:51:36 -0800

    draw knife in right place

--- lenstest/foucault.py
+++ lenstest/foucault.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 import lenstest.lenstest
 
 __all__ = ('gram',
-           'plot_ruling_and_screen',
+           'plot_gram',
+           'plot_knife_and_screen',
            'plot_lens_layout',
            'plot_mirror_layout')
 
@@ -28,13 +29,13 @@ def _transmitted(RoC, x_offset, z_offset, X, Y, conic=0, phi=0, mask=False, A=0)
 
     Args:
         RoC: radius of curvature of mirror [mm]
-        x_offset: knife edge displacement from optical axis [mm]
-        z_offset: axial distance from from true focus [mm]
+        x_offset: transverse knife edge displacement from optical axis [mm]
+        z_offset: axial knife edge offset from paraxial focus [mm]
         X, Y: grid of points to evaluate [mm]
         conic: conic constant or Schwartzchild constant [-]
-        phi: CCW rotation of knife edge from horizontal [radians]
+        phi: CCW rotation of knife edge from vertical [radians]
         mask: show knife edge without lens/mirror effects
-        A1: coefficient of spherical aberration
+        A: coefficient of spherical aberration
     Returns:
         1D boolean array describing points blocked by knife edge
     """
@@ -58,14 +59,14 @@ def _transmitted(RoC, x_offset, z_offset, X, Y, conic=0, phi=0, mask=False, A=0)
 def gram(D, RoC, x_offset, z_offset,
          conic=0, phi=0, N=100000, invert=False, on_grid=False, A=0, mask=False):
     """
-    Create a Foucogram for points on a grid or randomly selected.
+    Create points that miss the knife edge.
 
     This assumes that the point source of light is located at the center
     of the mirror madius of curvature, RoC.
 
-    The knife edge is displaced from the center of the mirror by offset.
+    The knife edge is displaced from the center of the mirror by x_offset.
 
-    The knife edge is oriented so lines are perpendicular to the x-axis
+    The knife edge is oriented its edge is vertical when phi=0.
 
     The conic section is specified by conic:
           conic = ∞ for surface that is flat
@@ -78,10 +79,10 @@ def gram(D, RoC, x_offset, z_offset,
     Args:
         D: diameter of mirror [mm]
         RoC: radius of curvature of mirror [mm]
-        x_offset: knife edge displacement from optical axis [mm]
-        z_offset: axial distance from from true focus [mm]
+        x_offset: transverse knife edge displacement from optical axis [mm]
+        z_offset: axial knife edge offset from paraxial focus [mm]
         conic: conic constant or Schwartzchild constant [-]
-        phi: CCW rotation of knife edge from horizontal [radians]
+        phi: CCW rotation of knife edge from vertical [radians]
         N: number of points to generate
         invert: boolean to draw dark or light areas
         on_grid: if False generate points on a grid
@@ -105,47 +106,104 @@ def gram(D, RoC, x_offset, z_offset,
     return x_image, y_image
 
 
-def plot_ruling_and_screen(D, RoC, x_offset, z_offset,
-                           conic=0, phi=0, init=True, A=0, on_grid=False):
-    """Plot the Foucault knife edge image."""
+def plot_gram(D, RoC, x_offset, z_offset,
+              conic=0, phi=0, A=0, on_grid=False):
+    """
+    Plot the Foucault knife edge image.
+    
+    The idea is to graph both the beam on the knife edge and the expected
+    projection on a screen located at the radius-of-curvature away from
+    the focus.  This allows rapid visualization or roughly how much
+    of the knife edge interacts with the screen.
+
+    The beam size is limited by the diffraction focus limit of the beam
+    (assuming a 1000nm wavelength).
+
+    Args:
+        D: diameter of mirror [mm]
+        RoC: radius of curvature of mirror [mm]
+        x_offset: transverse knife edge offset from optical axis [mm]
+        z_offset: axial knife edge offset from paraxial focus [mm]
+        conic: conic or Schwartzchild constant [-]
+        phi: CCW rotation of Ronchi ruling from vertical [radians]
+        init: set to False to allow updating plots
+        on_grid: if False generate points on a grid
+        invert: set to True to invert ruling
+
+    Returns:
+        fig: matplotlib Figure object representing the plot
+        ax: matplotlib Axes object representing the plot
+    """
     x, y = gram(D, RoC, x_offset, z_offset, conic=conic, phi=phi, A=A, on_grid=on_grid)
+
+    lenstest.lenstest.draw_circle(D / 2, color='blue')
+    plt.plot(x, y, 'o', markersize=0.1, color='white')
+    plt.ylim(-D / 2 * 1.2, D / 2 * 1.2)
+    plt.xlim(-D / 2 * 1.2, D / 2 * 1.2)
+    plt.xlabel("(mm)")
+    plt.ylabel("(mm)")
+    plt.text(0.5, 0.97,
+             "Mirror/Lens is %s " % lenstest.lenstest.conic_string(conic),
+             color='white',
+             ha='center', va='top',
+             transform=plt.gca().transAxes
+    )
+    plt.title("Screen is %.0f mm from Focus" % RoC)
+    plt.gca().set_aspect('equal')
+    plt.gca().set_facecolor("black")
+
+
+def plot_knife_and_screen(D, RoC, x_offset, z_offset,
+                           conic=0, phi=0, init=True, A=0, on_grid=False, invert=False):
+    """
+    Plot the Foucault knife edge image.
+    
+    The idea is to graph both the beam on the knife edge and the expected
+    projection on a screen located at the radius-of-curvature away from
+    the focus.  This allows rapid visualization or roughly how much
+    of the knife edge interacts with the screen.
+
+    The beam size is limited by the diffraction focus limit of the beam
+    (assuming a 1000nm wavelength).
+
+    Args:
+        D: diameter of mirror [mm]
+        RoC: radius of curvature of mirror [mm]
+        x_offset: transverse knife edge offset from optical axis [mm]
+        z_offset: axial knife edge offset from paraxial focus [mm]
+        conic: conic or Schwartzchild constant [-]
+        phi: CCW rotation of Ronchi ruling from vertical [radians]
+        init: set to False to allow updating plots
+        on_grid: if False generate points on a grid
+        invert: set to True to invert ruling
+
+    Returns:
+        fig: matplotlib Figure object representing the plot
+        ax: matplotlib Axes object representing the plot
+    """
 
     if init:
         fig, ax = plt.subplots(1, 2, figsize=(10,4))
     else:
         fig, ax = None, None
 
-    plt.subplot(1, 2, 2)
-    plt.gca().set_facecolor("black")
-    plt.plot(x, y, 'o', markersize=0.1, color='white')
-    plt.gca().set_aspect('equal')
-    lenstest.lenstest.draw_circle(D / 2, color='blue')
-    plt.ylim(-D / 2 * 1.2, D / 2 * 1.2)
-    plt.xlim(-D / 2 * 1.2, D / 2 * 1.2)
-    plt.title("D=%.1fmm, RoC=%.1fmm, K=%.2f" % (D, RoC, conic))
-    plt.xlabel("Projection Plane Coordinates (mm)")
-    plt.ylabel("Projection Plane Coordinates (mm)")
-    cs=lenstest.lenstest.conic_string(conic)
-    plt.text(0.5, 0.97,
-             "Mirror/Lens is %s " % cs,
-             color='white',
-             ha='center', va='top',
-             transform=plt.gca().transAxes
-    )
-    plt.title("Screen is %.0f mm from Focus" % RoC)
-
-    x, y = gram(D, RoC, x_offset, z_offset, phi=phi, mask=True, on_grid=on_grid)
+    # plot in the plane of the knife edge
     plt.subplot(1, 2, 1)
-    x, y = lenstest.lenstest.knife_polygon(D / 2, phi, x_offset)
-    r_spot = abs(z_offset * D / 2 / RoC)
-    lenstest.lenstest.draw_circle(r_spot, color='blue')
     plt.gca().set_aspect('equal')
-    plt.fill(x, y, color='black', alpha=0.8)
-    plt.plot(0, 0, 'bo')
-    plt.xlabel("Knife Plane Coordinates (mm)")
-    plt.ylabel("Knife Plane Coordinates (mm)")
+    
+    # draw the knife
+    x, y = lenstest.lenstest.knife_polygon(D / 2, phi, x_offset)
+    plt.fill(x, y, color='silver', alpha=0.8)
+    plt.plot(0, 0, 'b+', markersize=5)
 
-    size = max(r_spot * 4, abs(2*x_offset))
+    # Draw circle showing spotsize in the knife edge plane
+    r_geometric = abs(D / 2 / RoC * z_offset)        # mm
+    r_diffraction = 0.5 * 1e-6 * (RoC / 2) / D  # mm
+    r_spot = max(r_geometric, r_diffraction)
+    lenstest.lenstest.draw_circle(r_spot, color='blue')
+
+    # we always want to see the edge of the knife
+    size = max(r_spot * 2.2, abs(2*x_offset))
     plt.ylim(-size, size)
     plt.xlim(-size, size)
 
@@ -156,12 +214,44 @@ def plot_ruling_and_screen(D, RoC, x_offset, z_offset,
 #             transform=plt.gca().transAxes
 #    )
 
+    # label the plot
+    plt.xlabel("Knife x (mm)")
+    plt.ylabel("Knife y (mm)")
     ks = 'at'
     if z_offset < 0:
         ks = '%.3fmm before' % abs(z_offset)
     if z_offset > 0:
         ks = '%.3fmm after' % z_offset
-    plt.title("Knife edge is %s focus" % ks)
+    plt.title("%s focus" % ks)
+
+    # plot in the plane of the screen
+    plt.subplot(1, 2, 2)
+    plt.gca().set_facecolor("black")
+    plt.gca().set_aspect('equal')
+
+    # knife edge result
+    x, y = gram(D, RoC, x_offset, z_offset, conic=conic, phi=phi, A=A, on_grid=on_grid)
+    plt.plot(x, y, 'o', markersize=0.1, color='white')
+
+    # the circle for the projected circle from mirror
+    lenstest.lenstest.draw_circle(D / 2, color='blue')
+    size = D / 2 * 1.2
+    plt.ylim(-size, size)
+    plt.xlim(-size, size)
+
+    # add labels
+    plt.title("D=%.1fmm, RoC=%.1fmm, K=%.2f" % (D, RoC, conic))
+    plt.xlabel("Screen x (mm)")
+    plt.ylabel("Screen y (mm)")
+    cs=lenstest.lenstest.conic_string(conic)
+    plt.text(0.5, 0.97,
+             "Mirror/Lens is %s " % cs,
+             color='white',
+             ha='center', va='top',
+             transform=plt.gca().transAxes
+    )
+    plt.title("%.0fmm from Focus" % RoC)
+
     return fig, ax
 
 
@@ -172,8 +262,8 @@ def plot_lens_layout(D, RoC, x_offset, z_offset):
     Args:
         D: diameter of mirror or lens [mm]
         RoC: radius of curvature of mirror [mm]
-        x_offset: transverse offset of knife edge from optical axis [mm]
-        z_offset: axial offset of knife edge from true focus [mm]
+        x_offset: transverse knife edge offset from optical axis [mm]
+        z_offset: axial knife edge offset from paraxial focus [mm]
     Returns:
         fig: matplotlib Figure object representing the plot
         ax: matplotlib Axes object representing the plot
@@ -192,11 +282,12 @@ def plot_lens_layout(D, RoC, x_offset, z_offset):
     m = max(m,-m2)
 
     # marginal rays
-    plt.plot([-RoC, RoC], [D/2,-D/2], color='black', linewidth=1)
-    plt.plot([-RoC, RoC], [-D/2,D/2], color='black', linewidth=1)
+    plt.plot([-2*RoC,-RoC, RoC], [0,D/2,-D/2], color='black', linewidth=1)
+    plt.plot([-2*RoC,-RoC, RoC], [0,-D/2,D/2], color='black', linewidth=1)
+    plt.plot(-2*RoC, 0, 'ok', markersize=5)
 
     # ray touching the knife edge
-    plt.plot([-RoC, RoC], [-m*RoC, m*RoC], color='black', linewidth=1, linestyle='--')
+    plt.plot([-2*RoC,-RoC, RoC], [0,-m*RoC, m*RoC], color='black', linewidth=1, linestyle='--')
 
     # shade blocked light
     if z_offset < 0:
@@ -207,7 +298,7 @@ def plot_lens_layout(D, RoC, x_offset, z_offset):
                          [-z_offset*m2, -D/2], color='darkgray')
 
     # draw the lens
-    lenstest.lenstest.draw_lens(D, RoC)
+    lenstest.lenstest.draw_lens(D, RoC, -RoC)
     plt.text(-RoC, -D/2, 'lens/mirror', ha='right', rotation=90, color='blue')
 
 # focus plane
@@ -244,7 +335,7 @@ def plot_mirror_layout(D, RoC, x_offset, z_offset):
         D: diameter of mirror or lens [mm]
         RoC: radius of curvature of mirror [mm]
         x_offset: transverse offset of knife edge from optical axis [mm]
-        z_offset: axial offset of knife edge from true focus [mm]
+        z_offset: axial knife edge offset from paraxial focus [mm]
     Returns:
         fig: matplotlib Figure object representing the plot
         ax: matplotlib Axes object representing the plot
@@ -263,11 +354,12 @@ def plot_mirror_layout(D, RoC, x_offset, z_offset):
     m = max(m,-m2)
 
     # marginal rays
-    plt.plot([-RoC, RoC], [D/2,-D/2], color='black', linewidth=1)
-    plt.plot([-RoC, RoC], [-D/2,D/2], color='black', linewidth=1)
+    plt.plot(0, D/20, 'ok', markersize=5)
+    plt.plot([0,-RoC, RoC], [D/20, D/2,-D/2], color='black', linewidth=1)
+    plt.plot([0,-RoC, RoC], [D/20, -D/2,D/2], color='black', linewidth=1)
 
     # ray touching the knife edge
-    plt.plot([-RoC, RoC], [-m*RoC, m*RoC], color='black', linewidth=1, linestyle='--')
+    plt.plot([0,-RoC, RoC], [D/20,-m*RoC, m*RoC], color='black', linewidth=1, linestyle='--')
 
     # shade blocked light
     if z_offset < 0:
@@ -278,7 +370,7 @@ def plot_mirror_layout(D, RoC, x_offset, z_offset):
                          [-z_offset*m2, -D/2], color='darkgray')
 
     # draw the lens
-    lenstest.lenstest.draw_lens(D, RoC)
+    lenstest.lenstest.draw_mirror(D, RoC, vertex=-RoC)
     plt.text(-RoC, -D/2, 'lens/mirror', ha='right', rotation=90, color='blue')
 
 # focus plane
