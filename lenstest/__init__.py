1cbc92a1e7ac07a648d07654a7ef6a484546cebd
Scott Prahl <scott@prahls.com>
2024-05-08 13:01:44 -0700

    change import style

--- lenstest/__init__.py
+++ lenstest/__init__.py
@@ -7,10 +7,10 @@ Documentation and examples are at <https://lenstest.readthedocs.io>
 __version__ = '0.9.0'
 __author__ = 'Scott Prahl'
 __email__ = 'scott.prahl@oit.edu'
-__copyright__ = '2021-23, Scott Prahl'
+__copyright__ = '2021-24, Scott Prahl'
 __license__ = 'MIT'
 __url__ = 'https://github.com/scottprahl/lenstest'
 
 from .lenstest import *
-from . import ronchi
-from . import foucault
+from .ronchi import *
+from .foucault import *
